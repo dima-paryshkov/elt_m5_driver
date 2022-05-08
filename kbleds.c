d04c4102a8f94ef243b5ae1f056694a98205e1ce
Dmitry Paryshkoov <dmitry@paryshkov.ru>
2022-05-08 16:22:02 +0300

    feat control of keyboard using sysfs

--- kbleds.c
+++ kbleds.c
@@ -9,7 +9,11 @@
 #include <linux/kobject.h> 
 #include <linux/sysfs.h> 
 
+#define RESTORE_LEDS  0xFF
+
 static struct kobject *kbleds_kobject;
+
+struct tty_driver *kbleds_driver;
 
 /* the variable you want to be able to change */ 
 static int kbledstatus = 0;
@@ -21,7 +25,17 @@ static ssize_t kbledstatus_show(struct kobject *kobj, struct kobj_attribute *att
  
 static ssize_t kbledstatus_store(struct kobject *kobj, struct kobj_attribute *attr, char *buf, size_t count) 
 { 
-    sscanf(buf, "%du", &kbledstatus); 
+    sscanf(buf, "%du", &kbledstatus);
+
+    if (kbledstatus >= 0 && kbledstatus < 8)
+    {
+        (kbleds_driver->ops->ioctl) (vc_cons[fg_console].d->port.tty, KDSETLED, kbledstatus);
+    }
+    else
+    {
+        pr_info("kbleds: uncorrect value of arguments. It should be in [0, 7] \n");
+    }
+
     return count; 
 }
 
@@ -40,9 +54,26 @@ static int __init kbleds__init(void)
     error = sysfs_create_file(kbleds_kobject, &kbledstatus_attribute.attr); 
     if (error) 
     { 
-        pr_info("failed to create the myvariable file in /sys/kernel/mymodule\n"); 
-    } 
- 
+        pr_info("failed to create the myvariable file in /sys/kernel/kbleds\n"); 
+    }
+
+    printk(KERN_INFO "kbleds: loading\n");
+    printk(KERN_INFO "kbleds: fgconsole is %x\n", fg_console);
+    
+    int i;
+    for (i = 0; i < MAX_NR_CONSOLES; i++)
+    {
+        if (!vc_cons[i].d)
+            break;
+        printk(KERN_INFO "poet_atkm: console[%i/%i] #%i, tty %lx\n", i,
+               MAX_NR_CONSOLES, vc_cons[i].d->vc_num,
+               (unsigned long)vc_cons[i].d->port.tty);
+    }
+    printk(KERN_INFO "kbleds: finished scanning consoles\n");
+    
+    kbleds_driver = vc_cons[fg_console].d->port.tty->driver
+    printk(KERN_INFO "kbleds: tty driver magic %x\n", kbleds_driver->magic);
+
     return error; 
 } 
  
@@ -50,6 +81,7 @@ static void __exit kbleds__exit(void)
 { 
     pr_info("kbleds: Exit success\n"); 
     kobject_put(kbleds_kobject); 
+    (kbleds_driver->ops->ioctl) (vc_cons[fg_console].d->port.tty, KDSETLED, RESTORE_LEDS);
 } 
  
 module_init(kbleds__init); 
